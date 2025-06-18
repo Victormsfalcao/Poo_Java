22534dd1330396067fc6bb40a52d167fdc0a0419
Victormsfalcao <victorfalcao782@gmail.com>
2025-06-18 14:58:53 -0300

    Exemplos String

---
+++ aula03/src/aula03/Exemplo06.java
@@ -0,0 +1,15 @@
+package aula03;
+
+public class Exemplo06 {
+
+	public static void main(String[] args) {
+		String str = "Hello World";
+		String resultado = str.substring(6);
+		String resultado2 = str.substring(3,8);
+		
+		System.out.println(resultado);
+		System.out.println(resultado2);
+		
+	}
+
+}
---
+++ aula03/src/aula03/Exercicio09.java
@@ -0,0 +1,15 @@
+package aula03;
+import java.util.Arrays;
+
+public class Exercicio09 {
+
+	public static void main(String[] args) {
+		double numeros[] = {5,10,15,20,25};
+		
+		for(double i : numeros) {
+			System.out.println(i+" ");
+		}
+
+	}
+
+}
