72787703a0b18aa85ed8b108341edecbbcd57b6c
Victormsfalcao <victorfalcao782@gmail.com>
2025-06-18 14:18:56 -0300

    Adicição do exercicio 8 e correção do 7

--- aula03/src/aula03/Exercicio07.java
+++ aula03/src/aula03/Exercicio07.java
@@ -1,16 +1,19 @@
 package aula03;
 import java.util.Scanner;
+import java.util.Arrays;
 public class Exercicio07 {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int arrayA[] = new int [3];
+		int arrayA[] = new int [4];
 	
 		for(int i = 0;i <= arrayA.length;i++) {
 			System.out.printf("Lista A : Digite o %d valor: ", i + 1);
 			arrayA[i] = input.nextInt();
-			}
-		System.out.println(arrayA);
+			
+		
+		}
+	
 		
 		int arrayB[] = new int [4];
 		
@@ -31,6 +34,15 @@ public class Exercicio07 {
 			System.out.printf("Lista D : Digite o %d valor: ", i + 1);
 			arrayD[i] = input.nextInt();
 			}
+		
+		System.out.println(Arrays.toString(arrayA));
+		System.out.println(Arrays.toString(arrayB));
+		
+		System.out.println(Arrays.toString(arrayC));
+		System.out.println(Arrays.toString(arrayD));
+		
+		
+		
 		
 		input.close();
 	
---
+++ aula03/src/aula03/Exercicio08.java
@@ -0,0 +1,25 @@
+package aula03;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Exercicio08 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		float arrayAlunos[] = new float [5];
+		float soma = 0; 
+
+		for(int i = 0;i < arrayAlunos.length;i++) {
+			System.out.printf(" Insira a nota aluno %d : ", i + 1);
+			arrayAlunos[i] = input.nextFloat();
+			}
+		
+		for(int j = 0 ; j < arrayAlunos.length;j++ ) {
+			soma += arrayAlunos[j];
+			}
+		float media = soma / arrayAlunos.length;
+
+		System.out.printf("A média dos %d alunos é %.1f", arrayAlunos.length,media );
+		input.close();
+	}
+}
