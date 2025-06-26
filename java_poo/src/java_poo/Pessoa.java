3fc6a1e8d7398645865b6fcef142a1f5059f6761
Victormsfalcao <victorfalcao782@gmail.com>
2025-06-26 14:13:54 -0300

    Java poo - Exercicio1

---
+++ java_poo/src/java_poo/Pessoa.java
@@ -0,0 +1,17 @@
+package java_poo;
+
+public class Pessoa {
+	
+	String nome;
+	int idade;
+	double altura;
+	String genero;
+	String comida;
+	public void comer() {
+		
+		System.out.printf("%s Começou a comer %s. ", nome,comida);
+	}
+		
+	}
+
+
---
+++ java_poo/src/java_poo/Teste_Pessoa.java
@@ -0,0 +1,13 @@
+package java_poo;
+
+public class Teste_Pessoa {
+
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa();
+		p1.nome = "Vitico";
+		p1.comida = "Hamburguer";
+		p1.comer();
+	}
+
+}
